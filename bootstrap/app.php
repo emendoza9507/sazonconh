70357d86164bd2b5dcb16178e2ce071967d9c727
Eduardo Mendoza Campos <emendoza9507@gmail.com>
2025-03-14 06:41:58 -0400

    Error handlidn

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,30 +8,28 @@ use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Log;
 use Symfony\Component\CssSelector\Exception\InternalErrorException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
-        if(app()->environment('production')) {
-            $exceptions->respond(function (Response|RedirectResponse $response) use ($exceptions) {
-                if ($exceptions instanceof NotFoundHttpException) {
-                    return response()->redirectTo('/');
+        $exceptions->respond(function (Response|RedirectResponse $response) use ($exceptions) {
+            if ($exceptions instanceof NotFoundHttpException) {
+                return response()->redirectTo('/');
             }
 
-            if($exceptions instanceof InternalErrorException) {
+            if ($exceptions instanceof InternalErrorException) {
                 Log::error($exceptions->getMessage());
-                 return throw $exceptions;
-                }
+                return throw $exceptions;
+            }
 
-                return response()->redirectTo('/');
-            });
-        }
+            return response()->redirectTo('/');
+        });
     })->create();
